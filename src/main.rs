cf813790fda74337dd08bb250dbfb5c74f8424a7
HamxAnwar <hamzaanwar93@outlook.com>
2025-06-14 16:56:43 +0500

    The code takes IP address as command line argument and gives open ports.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 // This project makes a basic port scanner in Rust.
 // Usage: cargo run -- <ip_address>
 
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, TcpStream};
+use std::net::{IpAddr, SocketAddr, TcpStream};
+use std::str::FromStr;
 use std::time::Duration;
-use std::{env, process, thread};
+use std::{env, process};
 
 fn scan_port(target: &SocketAddr) {
     let stream = TcpStream::connect_timeout(&target, Duration::from_millis(1000));
     match stream {
-        Ok(stream) => println!("Port {} is open", target.port()),
-        Err(e) => {
-            println!("Error: {}", e);
-            process::exit(1);
-        }
+        Ok(_) => println!("Port {} is open", target.port()),
+        Err(_) => (), // The port is closed.
     }
 }
 
 fn main() {
+    let ipaddr = match env::args().nth(1) {
+        Some(value) => IpAddr::from_str(&value).expect("Please provide correct IP address."),
+        None => {
+            println!("Wrong command usage.");
+            println!("Usage: cargo run -- <ip_address>");
+            process::exit(1);
+        }
+    };
     // let ipaddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80);
     for port in 1..65536 {
-        scan_port(&SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::new(192, 168, 16, 119)),
-            port as u16,
-        ));
+        scan_port(&SocketAddr::new(ipaddr, port as u16));
     }
 }
