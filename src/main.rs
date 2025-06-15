e921fe5614867740f555c9b6740f48e56e9423bf
HamxAnwar <hamzaanwar93@outlook.com>
2025-06-15 20:57:05 +0500

    The scanner is in RUST. It gets IP as command line interface, and give ephermal + regular ports. Tried to remove ephermal ports by introducing peek function with stream but it skips many other ports which doesn't reply back

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@
 
 use std::net::{IpAddr, SocketAddr, TcpStream};
 use std::str::FromStr;
-use std::time::Duration;
-use std::{env, process};
+use std::time::{self, Duration};
+use std::{env, process, thread};
 
 fn scan_port(target: &SocketAddr) {
-    let stream = TcpStream::connect_timeout(&target, Duration::from_millis(1000));
+    let stream = TcpStream::connect_timeout(&target, Duration::from_millis(1));
     match stream {
         Ok(_) => println!("Port {} is open", target.port()),
         Err(_) => (), // The port is closed.
     }
 }
+
+/*
+I have tried to use peek with stream to remove the ephermal ports but it is seen to be not of any benefit.
+Since it requires the connection to send data back and in most cases, the data is not sent back.
+For example in case of CUPS port (631) which results in port dropage thought the port is open.
+*/
 
 fn main() {
     let ipaddr = match env::args().nth(1) {
@@ -23,8 +29,22 @@ fn main() {
             process::exit(1);
         }
     };
-    // let ipaddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80);
-    for port in 1..65536 {
-        scan_port(&SocketAddr::new(ipaddr, port as u16));
+
+    let mut handles = Vec::new();
+    let chunk_size = 6553;
+    for i in 0..10 {
+        let start = i * chunk_size + 1;
+        let end = std::cmp::min((i + 1) * chunk_size, 65535);
+
+        let handle = thread::spawn(move || {
+            for port in start..=end {
+                scan_port(&SocketAddr::new(ipaddr, port as u16));
+            }
+            thread::sleep(time::Duration::from_secs(1));
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
